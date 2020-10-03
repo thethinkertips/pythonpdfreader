85327bcc8aaab654dddda3f2269ca39f7690a1f1
thethinkertips <72246316+thethinkertips@users.noreply.github.com>
2020-10-03 13:50:55 +0530

    Create pdfreaderpy.py

---
+++ pdfreaderpy.py
@@ -0,0 +1,22 @@
+import pyttsx3
+import PyPDF2
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+book = open('yourpdffile.pdf', 'r')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+print(pages)
+
+
+# book.close()
+speak(pdfReader)
